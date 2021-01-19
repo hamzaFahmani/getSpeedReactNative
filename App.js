0fb8289411943cd15094042279093ce2278c4df9
hf0017 <64808190+hf0017@users.noreply.github.com>
2021-01-19 12:42:28 +0100

    First

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Vibration,
+  Text,
+  Alert,
+} from "react-native";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class Acceuil extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  _getPermissions = async () => {
+    const { status } = await Location.requestPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert("Error", "Location is not granted");
+      return;
+    }
+  };
+
+  _getLocation = async () => {
+    try {
+      const userLocation = await Location.getCurrentPositionAsync({});
+      console.log(userLocation.coords.speed);
+    } catch (e) {
+      console.log({ error: e });
+    }
+  };
+
+  render() {
+    this._getPermissions();
+    this._getLocation();
+    return (
+      <View style={styles.container}>
+        <StatusBar style="light" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
